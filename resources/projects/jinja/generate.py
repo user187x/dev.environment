33541cc210db7a7208abf0dd6d417fa29938b7b2
xxx <email>
2026-05-05 01:31:31 -0400

    Added python ruff installation scripts

--- resources/projects/jinja/generate.py
+++ resources/projects/jinja/generate.py
@@ -1,28 +1,27 @@
 import getpass
 from jinja2 import Template
 
+
 def main():
-    # Prompt for username
-    username = input("Enter NiFi Cluster Username: ")
-    
-    # Prompt securely for password
-    password = getpass.getpass("Enter NiFi Cluster Password: ")
+ # Prompt for username
+ username = input('Enter NiFi Cluster Username: ')
 
-    # Read the template file
-    with open('jinja-template.yaml', 'r') as file:
-        template = Template(file.read())
+ # Prompt securely for password
+ password = getpass.getpass('Enter NiFi Cluster Password: ')
 
-    # Render the template with the provided variables
-    rendered_yaml = template.render(
-        nifi_username=username,
-        nifi_password=password
-    )
+ # Read the template file
+ with open('jinja-template.yaml', 'r') as file:
+  template = Template(file.read())
 
-    # Write the result to the final configuration file
-    with open('nifi-cluster.yaml', 'w') as file:
-        file.write(rendered_yaml)
+ # Render the template with the provided variables
+ rendered_yaml = template.render(nifi_username=username, nifi_password=password)
 
-    print("✅ Successfully generated nifi-cluster.yaml")
+ # Write the result to the final configuration file
+ with open('nifi-cluster.yaml', 'w') as file:
+  file.write(rendered_yaml)
 
-if __name__ == "__main__":
-    main()
+ print('✅ Successfully generated nifi-cluster.yaml')
+
+
+if __name__ == '__main__':
+ main()
